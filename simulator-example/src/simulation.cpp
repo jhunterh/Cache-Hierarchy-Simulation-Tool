36b9dda4d9e56015f95968206433cd3ef36de61c
Tristan Jay <CapdinCrando@gmail.com>
2024-04-19 13:28:19 -0500

    Add simulator output file

--- simulator-example/src/simulation.cpp
+++ simulator-example/src/simulation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 #include "api.h"
@@ -9,11 +10,12 @@
 // Example simulation run
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cout << "Usage: ./SimulatorExample <config file>" << std::endl;
+        std::cout << "Usage: ./SimulatorExample <config file> <output file>" << std::endl;
         return 1;
     }
+
     // Api instantiation
     CacheHierarchySimulator::Api api;
 
@@ -35,43 +37,61 @@ int main(int argc, char** argv)
     std::cout << "Starting Simulation." << std::endl;
     std::vector<CacheHierarchySimulator::SystemStats> stats = api.runSimulation(instructionList);
 
-    for (auto& stat : stats)
+    // Open output file
+    std::ofstream outputFile(argv[2], std::ios::out);
+    if(!outputFile.good())
     {
-        for (auto& coreStat : stat.coreStats)
+        std::cerr << "Unable to open file: argv[2]" << std::endl;
+        return 1;
+    }
+
+    for (size_t systemIdx = 0; systemIdx < stats.size(); systemIdx++)
+    {
+        outputFile << "- System #" << +systemIdx << std::endl;
+        const auto& systemStat = stats.at(systemIdx);
+        outputFile << "    - System averageMemoryAccessTime " << systemStat.totalSystemStats.averageMemoryAccessTime << std::endl;
+        outputFile << "    - System readHits " << systemStat.totalSystemStats.readHits << std::endl;
+        outputFile << "    - System readMisses " << systemStat.totalSystemStats.readMisses << std::endl;
+        outputFile << "    - System writeHits " << systemStat.totalSystemStats.writeHits << std::endl;
+        outputFile << "    - System writeMisses " << systemStat.totalSystemStats.writeMisses << std::endl << std::endl;
+
+        const auto& coreStats = systemStat.coreStats;
+        size_t cacheIdx = 0;
+        for (size_t coreIdx = 0; coreIdx < coreStats.size(); coreIdx++)
         {
-            for (auto& cacheStat : coreStat.cacheStats)
+
+            outputFile << "    - Core #" << +coreIdx << std::endl;
+            const auto& coreStat = coreStats.at(coreIdx);
+            outputFile << "        - Core averageMemoryAccessTime " << coreStat.totalCoreStats.averageMemoryAccessTime << std::endl;
+            outputFile << "        - Core readHits " << coreStat.totalCoreStats.readHits << std::endl;
+            outputFile << "        - Core readMisses " << coreStat.totalCoreStats.readMisses << std::endl;
+            outputFile << "        - Core writeHits " << coreStat.totalCoreStats.writeHits << std::endl;
+            outputFile << "        - Core writeMisses " << coreStat.totalCoreStats.writeMisses << std::endl << std::endl;
+
+            const auto& cacheStats = coreStat.cacheStats;
+            for (cacheIdx = 0; cacheIdx < cacheStats.size(); cacheIdx++)
             {
-                std::cout << "L1 Cache Stats" << std::endl;
-                std::cout << "L1 Cache averageMemoryAccessTime " << cacheStat.averageMemoryAccessTime << std::endl;
-                std::cout << "L1 Cache readHits " << cacheStat.readHits << std::endl;
-                std::cout << "L1 Cache readMisses " << cacheStat.readMisses << std::endl;
-                std::cout << "L1 Cache writeHits " << cacheStat.writeHits << std::endl;
-                std::cout << "L1 Cache writeMisses " << cacheStat.writeMisses << std::endl << std::endl;
+                const auto& cacheStat = cacheStats.at(cacheIdx);
+                outputFile << "        - L" << +(cacheIdx + 1) <<  " Cache Stats" << std::endl;
+                outputFile << "            - Cache averageMemoryAccessTime " << cacheStat.averageMemoryAccessTime << std::endl;
+                outputFile << "            - Cache readHits " << cacheStat.readHits << std::endl;
+                outputFile << "            - Cache readMisses " << cacheStat.readMisses << std::endl;
+                outputFile << "            - Cache writeHits " << cacheStat.writeHits << std::endl;
+                outputFile << "            - Cache writeMisses " << cacheStat.writeMisses << std::endl << std::endl;
             }
-            std::cout << "Core Stats" << std::endl;
-            std::cout << "Core averageMemoryAccessTime " << coreStat.totalCoreStats.averageMemoryAccessTime << std::endl;
-            std::cout << "Core readHits " << coreStat.totalCoreStats.readHits << std::endl;
-            std::cout << "Core readMisses " << coreStat.totalCoreStats.readMisses << std::endl;
-            std::cout << "Core writeHits " << coreStat.totalCoreStats.writeHits << std::endl;
-            std::cout << "Core writeMisses " << coreStat.totalCoreStats.writeMisses << std::endl << std::endl;
         }
 
-        for (auto& cacheStat : stat.sharedCacheStats)
+        const auto& sharedCacheStats = systemStat.sharedCacheStats;
+        for (size_t sharedCacheIdx = 0; sharedCacheIdx < sharedCacheStats.size(); sharedCacheIdx++)
         {
-            std::cout << "Shared Cache Stats" << std::endl;
-            std::cout << "Shared Cache averageMemoryAccessTime " << cacheStat.averageMemoryAccessTime << std::endl;
-            std::cout << "Shared Cache readHits " << cacheStat.readHits << std::endl;
-            std::cout << "Shared Cache readMisses " << cacheStat.readMisses << std::endl;
-            std::cout << "Shared Cache writeHits " << cacheStat.writeHits << std::endl;
-            std::cout << "Shared Cache writeMisses " << cacheStat.writeMisses << std::endl << std::endl;
+            const auto& sharedCacheStat = sharedCacheStats.at(sharedCacheIdx);
+            outputFile << "    - Shared L" << +(sharedCacheIdx + cacheIdx + 1) << " Cache Stats" << std::endl;
+            outputFile << "        - Cache averageMemoryAccessTime " << sharedCacheStat.averageMemoryAccessTime << std::endl;
+            outputFile << "        - Cache readHits " << sharedCacheStat.readHits << std::endl;
+            outputFile << "        - Cache readMisses " << sharedCacheStat.readMisses << std::endl;
+            outputFile << "        - Cache writeHits " << sharedCacheStat.writeHits << std::endl;
+            outputFile << "        - Cache writeMisses " << sharedCacheStat.writeMisses << std::endl << std::endl;
         }
-
-        std::cout << "System Stats" << std::endl;
-        std::cout << "System averageMemoryAccessTime " << stat.totalSystemStats.averageMemoryAccessTime << std::endl;
-        std::cout << "System readHits " << stat.totalSystemStats.readHits << std::endl;
-        std::cout << "System readMisses " << stat.totalSystemStats.readMisses << std::endl;
-        std::cout << "System writeHits " << stat.totalSystemStats.writeHits << std::endl;
-        std::cout << "System writeMisses " << stat.totalSystemStats.writeMisses << std::endl << std::endl;
     }
 
     return 0;
