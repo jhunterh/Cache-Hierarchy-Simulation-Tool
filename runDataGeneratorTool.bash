62b34140e08a78aa2f26e1a7292f2ad5cd476daa
jhunterh <44873307+jhunterh@users.noreply.github.com>
2024-04-20 11:03:43 -0500

    add chs_home environment variable

--- runDataGeneratorTool.bash
+++ runDataGeneratorTool.bash
@@ -2,4 +2,4 @@
 echo "Creating data folder..."
 mkdir -p data
 echo "Running Generator..."
-$PIN_HOME/pin -t DataGenerator/obj-intel64/DataGenerator.so $@
+$PIN_HOME/pin -t $CHS_HOME/DataGenerator/obj-intel64/DataGenerator.so $@
